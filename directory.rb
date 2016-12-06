59ac7c9002a055d7b8a11376373991c8cf4f9f8f
Ben Vaughan-Jones <benvaughanjones@gmail.com>
2016-12-06 11:35:48 +0000

    Used methods in directory.rb

--- directory.rb
+++ directory.rb
@@ -13,9 +13,22 @@ students = [
   "Norman Bathes"
 ]
 
-#prints each line from the array using it's index
-students.each do |student|
-  puts student
+def print_header
+  puts "The students of Villains Academy"
+  puts "-----------------"
 end
 
-puts "Overall, we have #{students.count} great students"
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+#calling methods
+print_header
+print(students)
+print_footer(students)
